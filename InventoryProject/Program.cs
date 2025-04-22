8651d460c1a6525c66eae7879b39f8fb638dd51c
Carlos Ramos <carlosrc78@gmail.com>
2025-04-21 21:14:59 -0500

    Agregar archivos de proyecto.

---
+++ InventoryProject/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace InventoryProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Inventory Management System ===");
+
+            SistemaInventario sistema =  new SistemaInventario();
+            CargarDatosIniciales(sistema);  // Va a cargar toos los datos que vamos a nesecitar
+
+            // Menu de entrada
+
+            bool salir = false;
+
+            while (!salir)
+            {
+                Console.WriteLine("\nMenu Principal:");
+                Console.WriteLine("1. Gestión de empleados");
+                Console.WriteLine("2. Gestión de Productos");
+                Console.WriteLine("3. Movimientos de stock");
+                Console.WriteLine("4. Reportes");
+                Console.WriteLine("0. Salir");
+
+                Console.WriteLine("\n Seleccione una opción: ");
+                String opcion = Console.ReadLine();
+
+                try
+                {
+                    switch (opcion)
+                    {
+                        case "1":
+                            GestionEmpleados(sistema);
+                            break;
+                        case "2":
+                            GestionProductos(sistema);
+                            break;
+                        case "3":
+                            MovimientosStock(sistema);
+                            break;
+                        case "4":
+                            MostrarReportes(sistema);
+                            break;
+                        case "0":
+                            Console.WriteLine("Opción Invalida");
+                            break;
+                    }
+
+                }
+                catch (Exception ex) //throw on error
+                {
+
+                    throw;
+                }
+
+
+
+            }
+
+        }
+    }
+}
