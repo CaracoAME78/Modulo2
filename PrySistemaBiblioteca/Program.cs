8089f277abe14cca37af31a347268ea6be829314
Carlos Ramos <carlosrc78@gmail.com>
2025-05-02 19:31:54 -0500

    Creación del proyecto Reserva Restaurant

--- PrySistemaBiblioteca/Program.cs
+++ PrySistemaBiblioteca/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace PrySistemaBiblioteca
+﻿using PrySistemaBiblioteca.Modelos;
+
+namespace PrySistemaBiblioteca
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Biblioteca biblioteca = new Biblioteca();
+
+            // Registrar libros
+            biblioteca.AgregarLibro(new Libro("El Principito", "Antoine de Saint-Exupéry", "ISBN001"));
+            biblioteca.AgregarLibro(new Libro("Cien años de soledad", "Gabriel García Márquez", "ISBN002"));
+
+            // Registrar usuarios
+            biblioteca.RegistrarUsuario(new Usuario("Ana Pérez", "U001"));
+            biblioteca.RegistrarUsuario(new Usuario("Luis Gómez", "U002"));
+
+            // Mostrar libros disponibles
+            Console.WriteLine("Libros disponibles:");
+            biblioteca.ListarLibrosDisponibles();
+
+            // Prestar un libro
+            biblioteca.PrestarLibro("ISBN001", "U001");
+
+            // Mostrar libros disponibles después del préstamo
+            Console.WriteLine("\nLibros disponibles después del préstamo:");
+            biblioteca.ListarLibrosDisponibles();
+
+            Console.ReadKey();
         }
     }
 }
---
+++ ReservaRestaurant/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ReservaRestaurant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to our DaftDelicias Resevation System");
+
+            // Inicializar el sistema
+            var reservationService = new ReservationService(new ReservationRepository());
+            var tableService = new TableService(new TablaReporsitory());
+            var cusotmerService = new CustomerService(new CustomerRepository());
+            // Inicializar 
+            var ui = new ConsoleUI(reservationService, tableService, cusotmerService);
+
+            ui.run();
+        }
+    }
+}
