3dd56ceb21c00f4fcccd9075ffe25040ce77338f
Carlos Ramos <carlosrc78@gmail.com>
2025-05-03 18:44:24 -0500

    Creación de las clases Paciente, enum Sexo, Service PacienteService y de la interface IPacienteService

---
+++ PrySistemaLaboratorio/Models/Sexo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrySistemaLaboratorio.Models
+{
+    internal enum Sexo
+    {
+        Masculino,
+        Femenino
+    }
+}
